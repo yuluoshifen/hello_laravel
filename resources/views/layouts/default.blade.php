c1f23a1305313768eb3b027c8fd16890ba4003ba
Rainie <1948118433@qq.com>
2018-12-11 14:07:43 +0800

    Finish layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,14 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        
+        @include('layouts._footer')
+    </div>
+</div>
 </body>
 </html>
