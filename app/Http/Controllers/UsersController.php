ff4d64119be300e54bf39f5b51b096a9a0439e48
Rainie <1948118433@qq.com>
2018-12-11 17:58:26 +0800

    Finish sign-up

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /*
+     * 显示注册
+     */
     public function create()
     {
         return view('users.create');
+    }
+
+    /*
+     * 执行注册
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '注册成功！');
+
+        return redirect()->route('users.show', [$user]);
+    }
+
+    /*
+     * 用户信息
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
